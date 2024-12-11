5f5460c874b1e76e58013d9a602dcd274b50d19d
Pavel Pasechnik <ingle1337@gmail.com>
2024-12-11 22:34:36 +0500

    task5

---
+++ Tyuiu.PasechnikPA1.Sprint6.Task5.V18.Task/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.PasechnikPA1.Sprint6.Task5.V18.Lib;
+namespace Tyuiu.PasechnikPA1.Sprint6.Task5.V18.Task
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Valid()
+        {
+            string path = @"C:\Users\chazo\source\repos\Tyuiu.ChazovaSR.Sprint6\Tyuiu.ChazovaSR.Sprint6.Task5.V18\bin\Debug\InPutDataFileTask5V18.txt";
+            DataService ds = new DataService();
+            double[] wait = new double[] { -14.32, -7.84, 12.89, -1.57, -3.64, -13.26, -8.91, -17.77, 13.83,
+                12.76, 8.86, -1.49 };
+            double[] res = ds.LoadFromDataFile(path);
+            CollectionAssert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidFileExist()
+        {
+            string path = @"C:\Users\chazo\source\repos\Tyuiu.ChazovaSR.Sprint6\Tyuiu.ChazovaSR.Sprint6.Task5.V18\bin\Debug\InPutDataFileTask5V18.txt";
+
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
